46520b80636eb0f816a0d9ef1ccbca278ad45235
Jean <jean_henrique2@hotmail.com>
2025-02-04 11:13:13 -0300

    aprendendo string

---
+++ aula-01/app.js
@@ -0,0 +1,17 @@
+let age = 31
+let currentyear = 2019
+
+console.log (age, currentyear)
+
+age = 32
+console.log(age)
+
+
+const points = 100
+console.log(points)
+
+var score = 75
+console.log(score)
+
+
+
---
+++ aula-02/app.js
@@ -0,0 +1,32 @@
+//strings
+console.log('hello, world')
+
+const email = 'jean_henrique2@hotmail.com.br'
+
+console.log(email)
+
+
+//concatenação de strings
+const firstName = 'Nathan'
+const lastName = 'Drake'
+const fullName = firstName + ' '+ lastName
+
+console.log(fullName)
+
+// acessando caracteres
+console.log(fullName[2])
+
+//comprimento de uma string
+console.log(fullName.length)
+
+//métodos de string
+console.log(fullName.toUpperCase())
+
+const result = fullName.toLocaleLowerCase()
+
+console.log(result, fullName)
+
+const index = email.indexOf('@')
+
+console.log(index)
+
