364fc8af4a481c9a10b83b3967fea4df79539bbe
Jean <jean_henrique2@hotmail.com>
2025-02-07 09:58:29 -0300

    Exercicio 2

--- aula-01/app.js
+++ aula-01/app.js
@@ -1,3 +1,4 @@
+/*
 let age = 31
 let currentyear = 2019
 
@@ -12,6 +13,28 @@ console.log(points)
 
 var score = 75
 console.log(score)
+*/
 
+//inteiros e decimais
+const radius = 10
+const pi = 3.14
 
+// operadores aritmeticos
+const area = pi * radius ** 2
 
+// ordem de operacoes
+const crazyOperation = 5 * (10 - 3) ** 2
+
+//operadores de incremento e decremento
+let postlikes = 10
+
+//operadores addition, subtraction, multiplication e division assigment
+// postlikes /=
+
+// nan - a number
+// console.log(7/'oi')
+
+//cocatenacao de string com numero
+const likesmessage = 'o post tem ' + postlikes + ' likes.'
+
+console.log(likesmessage)
