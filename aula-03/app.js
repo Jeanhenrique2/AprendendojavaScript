ea405d9a7fcc283b2bb7d205989dd641c7acc763
Jean <jean_henrique2@hotmail.com>
2025-02-07 13:34:55 -0300

    aula 03 e exercicios

---
+++ aula-03/app.js
@@ -0,0 +1,18 @@
+/*Arrays
+let heroes = ['Batman', 'Catwoman', 'Iron Man']
+heroes[2] = 'spider-Man'
+const ages = [31, 25, 39, 40, 25]
+const randomArray = [' Parker', 'Diana', 19, 18]
+
+//Metodos de arrays
+const joinHeroes = heroes.join(' | ')
+const indexOf25 = ages.indexOf('25')
+const moreHeroes = heroes.concat(['Superman', 'Wolverine'])
+const pushToHeroes = heroes.push('Cyclops', 'Hulk')
+const popHeroes = heroes.pop ()
+console.log(heroes)
+*/
+
+let emptiness = null
+
+console.log(emptiness, emptiness + 3, `o valor é ${emptiness}`)
